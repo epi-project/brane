773706f7f8edae880e17dd9bd77dbf0ebd9ebee2
Lut99 <mullertim99@gmail.com>
2023-05-15 17:01:09 +0200

    Working on pushing to registry with auth
    Former-commit-id: 24f2bc8af08684ceeb4ce06e4d82d4d47e1a7e9f

--- brane-tsk/examples/cli.rs
+++ brane-tsk/examples/cli.rs
@@ -4,7 +4,7 @@
 //  Created:
 //    15 May 2023, 11:15:47
 //  Last edited:
-//    15 May 2023, 11:52:02
+//    15 May 2023, 16:49:54
 //  Auto updated?
 //    Yes
 // 
@@ -18,14 +18,15 @@ use std::path::PathBuf;
 use clap::{Parser, Subcommand};
 use console::style;
 use humanlog::{DebugMode, HumanLogger};
-use log::{error, info};
+use log::{debug, error, info};
 
 use brane_shr::errors::ErrorTrace as _;
 use specifications::address::Address;
 use specifications::container::Image;
+use specifications::package::PackageInfo;
 
 use brane_tsk::docker::ImageSource;
-use brane_tsk::k8s::resolve_image_source;
+use brane_tsk::k8s::{read_config_async, resolve_image_source, BasicAuth, Client, Config, Connection, ExecuteInfo, Job, JobHandle, RegistryAuth};
 
 
 /***** ARGUMENTS *****/
@@ -65,6 +66,10 @@ enum K8sSubcommand {
     /// Pushes an image to a local registry.
     #[clap(name = "push", about = "Pushes a local package (.tar file) to the given remote registry.")]
     Push(K8sPushArguments),
+
+    /// Launches a job with the given parameters.
+    #[clap(name = "launch", about = "Launches a given job on the given Kubernetes backend.")]
+    Launch(K8sLaunchArguments),
 }
 
 /// Defines the arguments to push a package to a local registry.
@@ -79,6 +84,43 @@ struct K8sPushArguments {
     /// The tag of the image to push.
     #[clap(name="TAG", help="The tag of the image (given as '<name>:<version>') to push.")]
     tag      : Image,
+
+    /// If given, ignores any certificates and junk when pushing containers.
+    #[clap(short, long, help="If given, makes the backend image pusher ignore certificates.")]
+    insecure : bool,
+    /// The user's username, if using basic auth.
+    #[clap(short, long, requires="password", help="If given, use a username/password pair to login to the registry. Note that this one must always appear with '--password'")]
+    username : Option<String>,
+    /// The user's password, if using basic auth.
+    #[clap(short, long, requires="username", help="If given, use a username/password pair to login to the registry. Note that this one must always appear with '--username'")]
+    password : Option<String>,
+}
+
+/// Defines the arguments to push a package to a local registry.
+#[derive(Debug, Parser)]
+struct K8sLaunchArguments {
+    /// Defines the path to the Kubernetes config to use to connect.
+    #[clap(name="K8S_CONFIG_PATH", help="The Kubernetes config YAML file that provides which cluster to connect to and how.")]
+    config   : PathBuf,
+    /// Defines the path to the image to launch.
+    #[clap(name="IMAGE_PATH", help="The image .tar file to push to the registry.")]
+    image    : PathBuf,
+    /// Defines the path to the package.yml to launch.
+    #[clap(name="PACKAGE_YML_PATH", help="The package.yml file that describes the container.")]
+    package  : PathBuf,
+    /// Defines the registry address to push to.
+    #[clap(name="REGISTRY", help="The address of the registry to push to.")]
+    registry : Address,
+
+    /// If given, ignores any certificates and junk when pushing containers.
+    #[clap(short, long, help="If given, makes the backend image pusher ignore certificates.")]
+    insecure : bool,
+    /// The user's username, if using basic auth.
+    #[clap(short, long, requires="password", help="If given, use a username/password pair to login to the registry. Note that this one must always appear with '--password'")]
+    username : Option<String>,
+    /// The user's password, if using basic auth.
+    #[clap(short, long, requires="username", help="If given, use a username/password pair to login to the registry. Note that this one must always appear with '--username'")]
+    password : Option<String>,
 }
 
 
@@ -102,8 +144,80 @@ async fn main() {
         CliSubcommand::K8s(k8s) => match k8s.subcommand {
             K8sSubcommand::Push(push) => {
                 info!("Pushing {} to {}...", push.path.display(), push.registry);
-                if let Err(err) = resolve_image_source(push.tag.clone(), ImageSource::Path(push.path.clone()), push.registry.clone()).await { error!("{}", err.trace()); std::process::exit(1); }
-                println!("Successfully pushed image {} to {}", style(push.path.display()).bold().blue(), style(format!("{}/v2/{}:{}", push.registry, push.tag.name, push.tag.version.unwrap())).bold().blue());
+
+                // Deduce the auth method from the input
+                let auth: Option<RegistryAuth> = match (push.username, push.password) {
+                    (Some(username), Some(password)) => Some(RegistryAuth::Basic(BasicAuth{ username, password })),
+                    (None, None)                     => None,
+
+                    // Anything else should never occur
+                    _ => { unreachable!(); },
+                };
+
+                // Push the image
+                let source: ImageSource = match resolve_image_source(&push.tag, ImageSource::Path(push.path.clone()), &push.registry, auth, push.insecure).await {
+                    Ok(source) => source,
+                    Err(err)   => { error!("{}", err.trace()); std::process::exit(1); },
+                };
+
+                // Done!
+                println!("Successfully pushed image {} to {}", style(push.path.display()).bold().blue(), style(source.into_registry()).bold().blue());
+            },
+
+            K8sSubcommand::Launch(launch) => {
+                info!("Launching image {} (package {}) to cluster through registry {}", launch.image.display(), launch.package.display(), launch.registry);
+
+                // Deduce the auth method from the input
+                let auth: Option<RegistryAuth> = match (launch.username, launch.password) {
+                    (Some(username), Some(password)) => Some(RegistryAuth::Basic(BasicAuth{ username, password })),
+                    (None, None)                     => None,
+
+                    // Anything else should never occur
+                    _ => { unreachable!(); },
+                };
+
+                // Load the Kubernetes config file
+                debug!("Loading Kubernetes config file '{}'...", launch.config.display());
+                let config: Config = match read_config_async(&launch.config).await {
+                    Ok(config) => config,
+                    Err(err)   =>{ error!("{}", err.trace()); std::process::exit(1); },
+                };
+
+                // Load the package YAML
+                debug!("Loading package.yml '{}'...", launch.package.display());
+                let package: PackageInfo = match PackageInfo::from_path(launch.package) {
+                    Ok(package) => package,
+                    Err(err)    => { error!("{}", err.trace()); std::process::exit(1); },
+                };
+
+                // Attempt to resolve the image file
+                debug!("Resolving image source '{}'...", launch.image.display());
+                let image: Image = Image::new(&package.name, Some(&package.version), None::<String>);
+                let source: ImageSource = match resolve_image_source(&image, ImageSource::Path(launch.image.clone()), launch.registry.clone(), auth, launch.insecure).await {
+                    Ok(source) => source,
+                    Err(err)   => { error!("{}", err.trace()); std::process::exit(1); },
+                };
+
+                // Now connect to the cluster
+                debug!("Connecting to cluster...");
+                let client: Client = match Client::new(config) {
+                    Ok(client) => client,
+                    Err(err)   => { error!("{}", err.trace()); std::process::exit(1); },
+                };
+                let conn: Connection<Job> = client.connect("default");
+
+                // Launch the job!
+                debug!("Spawning job...");
+                let handle: JobHandle = match conn.spawn(ExecuteInfo {
+                    name : "ABC".into(),
+                    image,
+                    image_source : source,
+
+                    command : vec![],
+                }).await {
+                    Ok(handle) => handle,
+                    Err(err)   => { error!("{}", err.trace()); std::process::exit(1); },
+                };
             },
         },
     }
--- brane-tsk/src/k8s.rs
+++ brane-tsk/src/k8s.rs
@@ -4,7 +4,7 @@
 //  Created:
 //    08 May 2023, 13:01:23
 //  Last edited:
-//    15 May 2023, 11:55:11
+//    15 May 2023, 16:53:44
 //  Auto updated?
 //    Yes
 // 
@@ -16,20 +16,26 @@ use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FResult};
 use std::path::{Path, PathBuf};
 use std::process::{Command, ExitStatus, Output, Stdio};
+use std::str::FromStr as _;
 
 pub use kube::Config;
 pub use k8s_openapi::api::core::v1::Pod;
 pub use k8s_openapi::api::batch::v1::Job;
+use enum_debug::EnumDebug;
 use hex_literal::hex;
 use k8s_openapi::NamespaceResourceScope;
-use kube::api::{Api, Resource};
+use kube::api::{Api, PostParams, Resource};
 use kube::config::{Kubeconfig, KubeConfigOptions};
 use log::{debug, info, warn};
+use rand::Rng as _;
+use rand::distributions::Alphanumeric;
+use serde_json::json;
 use tokio::fs as tfs;
 
 use brane_shr::fs::{download_file_async, set_executable, unarchive_async, DownloadSecurity};
 use specifications::address::Address;
 use specifications::container::Image;
+use specifications::version::Version;
 
 use crate::docker::ImageSource;
 
@@ -128,26 +134,34 @@ impl Error for CraneError {
     }
 }
 
+
+
 /// Defines errors that occur when resolving an image source (i.e., pushing to a registry).
 #[derive(Debug)]
 pub enum ResolveError {
     /// Failed to download the crane client.
     CraneExe{ err: CraneError },
-    /// Failed to run the command to push the image to a remote registry.
+    /// Failed to launch the command to login to a registry.
+    LaunchLogin{ what: Command, err: std::io::Error },
+    /// Failed to launch the command to push the image to a remote registry.
     LaunchPush{ what: Command, err: std::io::Error },
-    /// The push command was run successfully, but failed.
+    /// The login command was launched successfully, but failed.
+    LoginFailure{ registry: Address, err: Box<Self> },
+    /// The push command was launched successfully, but failed.
     PushFailure{ path: PathBuf, image: String, err: Box<Self> },
-    /// The push command failed.
-    PushCommandFailure{ what: Command, status: ExitStatus, stdout: String, stderr: String },
+    /// Some command failed.
+    CommandFailure{ what: Command, status: ExitStatus, stdout: String, stderr: String },
 }
 impl Display for ResolveError {
     fn fmt(&self, f: &mut Formatter<'_>) -> FResult {
         use ResolveError::*;
         match self {
-            CraneExe{ .. }                                     => write!(f, "Failed to download `crane` registry client"),
-            LaunchPush{ what, .. }                             => write!(f, "Failed to launch command '{:?}' to push image", what),
-            PushFailure{ path, image, .. }                     => write!(f, "Failed to push image '{}' to '{}'", path.display(), image),
-            PushCommandFailure{ what, status, stdout, stderr } => write!(f, "Command '{:?}' failed with exit code {}\n\nstdout:\n{}\n{}\n{}\n\nstderr:\n{}\n{}\n{}\n\n", what, status.code().unwrap_or(-1), (0..80).map(|_| '-').collect::<String>(), stdout, (0..80).map(|_| '-').collect::<String>(), (0..80).map(|_| '-').collect::<String>(), stderr, (0..80).map(|_| '-').collect::<String>()),
+            CraneExe{ .. }                                 => write!(f, "Failed to download `crane` registry client"),
+            LaunchLogin{ what, .. }                        => write!(f, "Failed to launch command '{what:?}' to login to remote registry"),
+            LaunchPush{ what, .. }                         => write!(f, "Failed to launch command '{what:?}' to push image"),
+            LoginFailure{ registry, .. }                   => write!(f, "Failed to login to remote registry '{registry}'"),
+            PushFailure{ path, image, .. }                 => write!(f, "Failed to push image '{}' to '{}'", path.display(), image),
+            CommandFailure{ what, status, stdout, stderr } => write!(f, "Command '{:?}' failed with exit code {}\n\nstdout:\n{}\n{}\n{}\n\nstderr:\n{}\n{}\n{}\n\n", what, status.code().unwrap_or(-1), (0..80).map(|_| '-').collect::<String>(), stdout, (0..80).map(|_| '-').collect::<String>(), (0..80).map(|_| '-').collect::<String>(), stderr, (0..80).map(|_| '-').collect::<String>()),
         }
     }
 }
@@ -156,9 +170,11 @@ impl Error for ResolveError {
         use ResolveError::*;
         match self {
             CraneExe{ err }          => Some(err),
+            LaunchLogin{ err, .. }   => Some(err),
             LaunchPush{ err, .. }    => Some(err),
+            LoginFailure{ err, .. }  => Some(err),
             PushFailure{ err, .. }   => Some(err),
-            PushCommandFailure{ .. } => None,
+            CommandFailure{ .. } => None,
         }
     }
 }
@@ -224,13 +240,14 @@ impl Error for ClientError {
 /// Defines errors that occur when working with connections.
 #[derive(Debug)]
 pub enum ConnectionError {
-    Temp,
+    /// Failed to spawn the Kubernetes job
+    CreateJob{ name: String, version: Version, id: String, err: kube::Error },
 }
 impl Display for ConnectionError {
     fn fmt(&self, f: &mut Formatter<'_>) -> FResult {
         use ConnectionError::*;
         match self {
-            Temp => write!(f, "TEMP"),
+            CreateJob { name, version, id, .. } => write!(f, "Failed to launch package {}:{} as Kubernetes job with ID '{}'", name, version, id),
         }
     }
 }
@@ -238,7 +255,7 @@ impl Error for ConnectionError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         use ConnectionError::*;
         match self {
-            Temp => None,
+            CreateJob { err, .. } => Some(err),
         }
     }
 }
@@ -312,6 +329,59 @@ async fn ensure_crane_exe(path: impl AsRef<Path>, temp_dir: impl AsRef<Path>) ->
 
 
 
+/// Creates a Kubernetes job description file that we use to launch a job.
+/// 
+/// # Arguments
+/// - `einfo`: The [`ExecuteInfo`] struct that describes the job.
+/// 
+/// # Returns
+/// A tuple of the ID of this job, and the actual [`Job`] struct describing the job.
+fn create_k8s_job(einfo: ExecuteInfo) -> (String, Job) {
+    // Generate an identifier for this job.
+    let id: String = format!("{}-{}-{}", einfo.image.name, einfo.image.version.map(|v| v.to_string().replace('.', "_")).unwrap_or("latest".into()), rand::thread_rng().sample_iter(Alphanumeric).take(8).map(char::from).collect::<String>());
+
+    // Create the Job
+    let job: Job = serde_json::from_value(json!({
+        // Define the kind of this YAML file
+        "apiVersion": "batch/v1",
+        "kind": "Job",
+
+        // Define the job ID
+        "metadata": {
+            "name": id,
+        },
+
+        // Now define the rest of the job
+        "spec": {
+            "backoffLimit": 3,
+            "ttlSecondsAfterFinished": 120,
+            "template": {
+                "spec": {
+                    "containers": [{
+                        "name": id,
+                        "image": einfo.image_source.into_registry(),
+                        "args": einfo.command,
+                        // "env": <>,
+                        "securityContext": {
+                            "capabilities": {
+                                "drop": ["all"],
+                                "add": ["SYS_TIME"],
+                            },
+                            "privileged": false,
+                        },
+                    }],
+                    "restartPolicy": "Never",
+                }
+            }
+        },
+    })).unwrap();
+
+    // Now we can generate the struct efficiently using the JSON macro.
+    (id, job)
+}
+
+
+
 
 
 /***** AUXILLARY FUNCTIONS *****/
@@ -359,29 +429,70 @@ pub async fn read_config_async(path: impl AsRef<Path>) -> Result<Config, ConfigE
 /// - `image`: The image to push. This will determine its name and tag in the container registry.
 /// - `source`: The [`ImageSource`] to resolve.
 /// - `registry`: The address of the registry to upload the image to if necessary.
+/// - `auth`: Any method of authentication that we need for the registry.
+/// - `insecure`: If true, we pass the '--insecure' flag to the `crane` executable (i.e., ignore SSL certs and such).
 /// 
 /// # Returns
 /// Another [`ImageSource`] that is the resolved version of `image`.
 /// 
 /// # Errors
 /// This function may error if the given `image` was an [`ImageSource::Path`], and we failed to upload the image.
-pub async fn resolve_image_source(name: Image, source: ImageSource, registry: Address) -> Result<ImageSource, ResolveError> {
+pub async fn resolve_image_source(name: impl AsRef<Image>, source: impl Into<ImageSource>, registry: impl AsRef<Address>, auth: Option<RegistryAuth>, insecure: bool) -> Result<ImageSource, ResolveError> {
+    let name     : &Image      = name.as_ref();
+    let source   : ImageSource = source.into();
+    let registry : &Address    = registry.as_ref();
+    info!("Resolving image by maybe pushing it to a registry");
+
     // Only resolve if we're a local file
     let path: PathBuf = match source {
         ImageSource::Path(path)    => path,
-        ImageSource::Registry(reg) => { return Ok(ImageSource::Registry(reg)); },
+        ImageSource::Registry(reg) => {
+            debug!("Nothing to resolve (image is already a registry image; {reg})");
+            return Ok(ImageSource::Registry(reg));
+        },
     };
+    debug!("Resolving local image '{}' to a registry image @ '{}'", path.display(), registry);
 
     // Deduce the path to the registry
-    let registry: String = format!("{registry}/v2/{}:{}", name.name, if let Some(version) = &name.version { version } else { "latest" });
+    let address: String = format!("{registry}/v2/{}:{}", name.name, if let Some(version) = &name.version { version } else { "latest" });
 
     // Next, ensure the crane executable exists
+    debug!("Ensuring `crane` exists...");
     if let Err(err) = ensure_crane_exe(CRANE_PATH, "/tmp").await { return Err(ResolveError::CraneExe{ err }); }
 
+    // If there is any auth, run the command first
+    match auth {
+        Some(RegistryAuth::Basic(basic)) => {
+            info!("Using basic auth to login to registry");
+
+            // Prepare the login command in `crane`
+            debug!("Logging in to registry as user '{}'...", basic.username);
+            let mut cmd: Command = Command::new(CRANE_PATH);
+            cmd.args(["auth", "login", "-u"]);
+            cmd.arg(basic.username);
+            cmd.arg("-p");
+            cmd.arg(basic.password);
+            cmd.arg(registry.to_string());
+            cmd.stdout(Stdio::piped());
+            cmd.stderr(Stdio::piped());
+
+            // Run it
+            let output: Output = match cmd.output() {
+                Ok(output) => output,
+                Err(err)   => { return Err(ResolveError::LaunchLogin { what: cmd, err }); },
+            };
+            if !output.status.success() { return Err(ResolveError::LoginFailure { registry: registry.clone(), err: Box::new(ResolveError::CommandFailure { what: cmd, status: output.status, stdout: String::from_utf8_lossy(&output.stdout).into(), stderr: String::from_utf8_lossy(&output.stderr).into() }) }); }        
+        },
+
+        None => {},
+    }
+
     // Next up, prepare to launch crane with the tarball path
+    debug!("Pushing image using `crane`...");
     let mut cmd: Command = Command::new(CRANE_PATH);
     cmd.args(["push", path.display().to_string().as_str()]);
-    cmd.arg(&registry);
+    cmd.arg(&address);
+    if insecure { cmd.arg("--insecure"); }
     cmd.stdout(Stdio::piped());
     cmd.stderr(Stdio::piped());
 
@@ -390,10 +501,10 @@ pub async fn resolve_image_source(name: Image, source: ImageSource, registry: Ad
         Ok(output) => output,
         Err(err)   => { return Err(ResolveError::LaunchPush { what: cmd, err }); },
     };
-    if !output.status.success() { return Err(ResolveError::PushFailure { path, image: registry, err: Box::new(ResolveError::PushCommandFailure { what: cmd, status: output.status, stdout: String::from_utf8_lossy(&output.stdout).into(), stderr: String::from_utf8_lossy(&output.stderr).into() }) }); }
+    if !output.status.success() { return Err(ResolveError::PushFailure { path, image: address, err: Box::new(ResolveError::CommandFailure { what: cmd, status: output.status, stdout: String::from_utf8_lossy(&output.stdout).into(), stderr: String::from_utf8_lossy(&output.stderr).into() }) }); }
 
     // Done
-    Ok(ImageSource::Registry(registry))
+    Ok(ImageSource::Registry(address))
 }
 
 
@@ -424,6 +535,21 @@ pub struct ExecuteInfo {
     pub command      : Vec<String>,
 }
 
+/// Defines methods of authentication for registries.
+#[derive(Clone, Debug, EnumDebug)]
+pub enum RegistryAuth {
+    /// It needs a username and a password.
+    Basic(BasicAuth),
+}
+/// Defines the Docker registry basic authentication scheme.
+#[derive(Clone, Debug)]
+pub struct BasicAuth {
+    /// The username of the user.
+    pub username : String,
+    /// The password of the user.
+    pub password : String,
+}
+
 
 
 
@@ -433,9 +559,7 @@ pub struct ExecuteInfo {
 #[derive(Clone)]
 pub struct Client {
     /// A Kubernetes config to wrap around.
-    client           : kube::Client,
-    /// The registry address which we use to transfer images to Kubernetes.
-    registry_address : Address,
+    client : kube::Client,
 }
 
 impl Client {
@@ -443,7 +567,6 @@ impl Client {
     /// 
     /// # Arguments
     /// - `config`: The [`Config`] that we use to known to which cluster to connect and how.
-    /// - `registry_address`: The address of the Docker Registry that we can use to temporarily upload Docker images.
     /// 
     /// # Returns
     /// A new Client instance that can be used to connect to the cluster described in the given config.
@@ -451,7 +574,7 @@ impl Client {
     /// # Errors
     /// This function errors if we failed to create a [`kube::Client`] from the given `config`.
     #[inline]
-    pub fn new(config: impl Into<Config>, registry_address: impl Into<Address>) -> Result<Self, ClientError> {
+    pub fn new(config: impl Into<Config>) -> Result<Self, ClientError> {
         // Attempt to create a client from the given config
         let client: kube::Client = match kube::Client::try_from(config.into()) {
             Ok(client) => client,
@@ -461,7 +584,6 @@ impl Client {
         // Return ourselves with the client
         Ok(Self {
             client,
-            registry_address : registry_address.into(),
         })
     }
 
@@ -469,14 +591,13 @@ impl Client {
     /// 
     /// # Arguments
     /// - `path`: The [`Path`]-like to parse the Kubernetes config from.
-    /// - `registry_address`: The address of the Docker Registry that we can use to temporarily upload Docker images.
     /// 
     /// # Returns
     /// A new Client instance that can be used to connect to the cluster described in the given config.
     /// 
     /// # Errors
     /// This function may error if we failed to parse the given file or if we failed to create a [`kube::Client`] from the given `config`.
-    pub async fn from_path_async(path: impl AsRef<Path>, registry_address: impl Into<Address>) -> Result<Self, ClientError> {
+    pub async fn from_path_async(path: impl AsRef<Path>) -> Result<Self, ClientError> {
         // Attempt to load the configuration file
         let config: Config = match read_config_async(path).await {
             Ok(config) => config,
@@ -492,7 +613,6 @@ impl Client {
         // Return ourselves with the client
         Ok(Self {
             client,
-            registry_address : registry_address.into(),
         })
     }
 
@@ -512,8 +632,7 @@ impl Client {
     pub fn connect<R: Resource<Scope = NamespaceResourceScope>>(&self, namespace: impl AsRef<str>) -> Connection<R> where R::DynamicType: Default {
         // We create the requested API interface and return that
         Connection {
-            api              : Api::namespaced(self.client.clone(), namespace.as_ref()),
-            registry_address : &self.registry_address,
+            api : Api::namespaced(self.client.clone(), namespace.as_ref()),
         }
     }
 }
@@ -534,14 +653,12 @@ impl Debug for Client {
 /// # Generic arguments
 /// - `K`: 
 #[derive(Debug)]
-pub struct Connection<'c, R> {
-    /// The [`API`] abstraction with which we connect.
-    api              : Api<R>,
-    /// The registry address which we use to transfer images to Kubernetes.
-    registry_address : &'c Address,
+pub struct Connection<R> {
+    /// The [`Api`] abstraction with which we connect.
+    api : Api<R>,
 }
 
-impl<'c> Connection<'c, Job> {
+impl Connection<Job> {
     /// Launches a given job in Kubernetes.
     /// 
     /// # Arguments
@@ -552,14 +669,23 @@ impl<'c> Connection<'c, Job> {
     /// 
     /// # Errors
     /// This function errors if we failed to push the container to the local registry (if it was a file), connect to the cluster or if Kubernetes failed to launch the job.
-    pub async fn spawn<'s>(&'s self, einfo: ExecuteInfo) -> Result<JobHandle<'c, 's>, ConnectionError> {
+    pub async fn spawn(&self, einfo: ExecuteInfo) -> Result<JobHandle, ConnectionError> {
         info!("Spawning package task '{}' from '{}' on Kubernetes backend", einfo.name, einfo.image);
 
         // Assert the container has been uploaded
         if !matches!(einfo.image_source, ImageSource::Registry(_)) { panic!("Non-Registry ImageSource must have been resolved before calling Connection::spawn"); }
 
+        // Prepare the Kubernetes config file.
+        let image: Image = einfo.image.clone();
+        let (id, job): (String, Job) = create_k8s_job(einfo);
+
+        // Submit the job
+        if let Err(err) = self.api.create(&PostParams::default(), &job).await {
+            return Err(ConnectionError::CreateJob{ name: image.name, version: image.version.map(|v| Version::from_str(&v).ok()).flatten().unwrap_or(Version::latest()), id, err });
+        }
+
         // Done
-        Ok(JobHandle{ connection: self })
+        Ok(JobHandle{ connection: self, id })
     }
 }
 
@@ -567,7 +693,9 @@ impl<'c> Connection<'c, Job> {
 
 /// Represents a job that is currently running within a Kubernetes cluster.
 #[derive(Debug)]
-pub struct JobHandle<'c1, 'c2> {
+pub struct JobHandle<'c> {
     /// The connection of which we are a part.
-    connection : &'c2 Connection<'c1, Job>,
+    connection : &'c Connection<Job>,
+    /// The ID of the job we manage.
+    id         : String,
 }
