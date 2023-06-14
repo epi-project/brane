6b2215e1e3582aaf7d948548358ca4471f5b29a6
Lut99 <mullertim99@gmail.com>
2023-06-14 14:18:15 +0200

    Added boilerplate for C-wrappers
    Former-commit-id: ac1cd1fa9c2ea6e52997991a64c176564221be2f

---
+++ brane-ast-c/include/brane-ast.h
@@ -0,0 +1,20 @@
+/* BRANE AST.h
+ *   by Lut99
+ *
+ * Created:
+ *   14 Jun 2023, 11:49:07
+ * Last edited:
+ *   14 Jun 2023, 11:50:43
+ * Auto updated?
+ *   Yes
+ *
+ * Description:
+ *   Defines the headers of the `libbrane-ast` library.
+**/
+
+#ifndef BRANE_AST_H
+#define BRANE_AST_H
+
+
+
+#endif
--- brane-ast-c/src/lib.rs
+++ brane-ast-c/src/lib.rs
@@ -1,17 +1,21 @@
-pub fn add(
-    left: usize,
-    right: usize,
-) -> usize {
-    left + right
-}
+//  LIB.rs
+//    by Lut99
+// 
+//  Created:
+//    14 Jun 2023, 11:48:13
+//  Last edited:
+//    14 Jun 2023, 11:50:51
+//  Auto updated?
+//    Yes
+// 
+//  Description:
+//!   Wrapper around `brane-ast` that provides C-bindings. This allows the
+//!   BraneScript compiler to be used in `brane-ide`.
+//!   
+//!   The basics of how to do this are followed from:
+//!   http://blog.asleson.org/2021/02/23/how-to-writing-a-c-shared-library-in-rust/
+// 
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+/***** LIBRARY *****/
+
