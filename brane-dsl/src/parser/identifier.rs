68c82e24af8d54c770bde5c871eef3db59d71e8c
Daniel Voogsgerd <daniel@voogsgerd.nl>
2024-10-04 11:53:24 +0200

    refactor: Resolved elided lifetime has a name warnings

--- brane-dsl/src/parser/identifier.rs
+++ brane-dsl/src/parser/identifier.rs
@@ -31,7 +31,7 @@ use crate::tag_token;
 ///
 /// # Returns
 /// The remaining list of tokens and the parsed Identifier if there was anything to parse. Otherwise, a `nom::Error` is returned (which may be a real error or simply 'could not parse').
-pub fn parse<'a, E: ParseError<Tokens<'a>> + ContextError<Tokens<'a>>>(input: Tokens<'a>) -> IResult<Tokens, Identifier, E> {
+pub fn parse<'a, E: ParseError<Tokens<'a>> + ContextError<Tokens<'a>>>(input: Tokens<'a>) -> IResult<Tokens<'a>, Identifier, E> {
     trace!("Attempting to parse identifier");
     comb::map(tag_token!(Token::Ident), |t| Identifier::new(t.tok[0].as_string(), t.tok[0].inner().into())).parse(input)
 }
